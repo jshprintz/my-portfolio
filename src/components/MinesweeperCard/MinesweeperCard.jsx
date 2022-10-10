5f39c61f8bcfcd1e403cf6017c94c96f0f217be1
jshprintz <jshprintz@gmail.com>
2022-10-09 20:08:57 -0700

    cards centered on screen. haiku and minesweeper display up

--- src/components/MinesweeperCard/MinesweeperCard.jsx
+++ src/components/MinesweeperCard/MinesweeperCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 
-const HaikuCard = () => (
+const MinesweeperCard = () => (
   
   <Card raised href="/" centered style={{ width: 350 }}>
-    <img src="https://i.imgur.com/2ILCi8H.png" alt="minesweeper"/>
+    <img src="https://i.imgur.com/V0XIXJ1.png" alt="Haiku" />
     <Card.Content>
-      <Card.Header>Haiku For You</Card.Header>
+      <Card.Header>Mega Minesweeper</Card.Header>
       <Card.Meta>Created 2022</Card.Meta>
       <Card.Description>
-        A full MERN stack social media application designed around haikus
+        A browser-based game made with only HTML, CSS, and JavaScript
       </Card.Description>
     </Card.Content>
   </Card>
 );
 
-export default HaikuCard;
+export default MinesweeperCard;
--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
-import { Card } from 'semantic-ui-react'
-import HaikuCard from '../../components/HaikuCard/HaikuCard'
-import "./PortfolioPage.css"
+import { Card } from "semantic-ui-react";
+import HaikuCard from "../../components/HaikuCard/HaikuCard";
+import MinesweeperCard from "../../components/MinesweeperCard/MinesweeperCard";
+import "./PortfolioPage.css";
 
 function PortfolioPage() {
   return (
     <>
       <NavBar />
       <div className="cards">
-      <h1>This is the portfolio page</h1>
-      <Card.Group itemsPerRow={2} stackable >
-        <HaikuCard />
-      </Card.Group>
+        <h1>This is the portfolio page</h1>
+        <Card.Group itemsPerRow={2} stackable centered>
+          <HaikuCard />
+          <MinesweeperCard />
+        </Card.Group>
       </div>
     </>
   );
