c108eb5d53b0cd52e6e561af3c29cfd18a117cbf
jshprintz <jshprintz@gmail.com>
2022-10-11 13:21:59 -0700

    need to find a way to shrink badges

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -18,6 +18,11 @@ function HomePage() {
       </div>
       <br />
       <p className="intro-text-skills fourth-animation">DB</p>
+      <img
+        src="https://camo.githubusercontent.com/29e7fc6c62f61f432d3852fbfa4190ff07f397ca3bde27a8196bcd5beae3ff77/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f706f7374677265732d2532333331363139322e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d706f737467726573716c266c6f676f436f6c6f723d7768697465"
+        alt="PostgreSQL"
+        className="badge"
+      />
       <br />
       <p className="intro-text-skills fifth-animation">
         Platforms, Frameworks, and Libraries
