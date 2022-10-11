dc11343cea34ff12d7eefa09f4afad3005ddca6d
jshprintz <jshprintz@gmail.com>
2022-10-10 18:07:05 -0700

    added about me

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import "./AboutPage.css";
 
 function AboutPage() {
   return (
     <>
       <NavBar />
-      <h1>This is the about page</h1>
+      <h2>My Story</h2>
+      <p>
+        {${`My passion for programming was sparked when I was in high school. I
+        created a game called “Perfect Pump” on the TI-83+ calculator. The game
+        had a number displayed in currency, and the goal was to press a button
+        and stop it perfectly on $20. The game was simple, but everyone really
+        enjoyed it, and I vividly remember loving the feeling of making a
+        program that everyone else used. I went on to take courses in C++ and
+        Java, but didn’t pursue it as a career until much later in life. I
+        recently helped construct a VBA macro for my former company which was
+        deployed to over 300+ independent contractors. After a short time at the
+        company it was obvious that many of our tasks were repetitive and needed
+        automation. I took it upon myself to write this macro in an effort to
+        increase efficiency, and we ended up implementing it company-wide.
+        Throughout writing it, I taught myself object-oriented programming
+        concepts, and reinvigorated my love affair with programming. I unlocked
+        all the same feelings I had in high school after I made “Perfect Pump”.
+        I enrolled into the General Assembly Software Engineering Immersive
+        course because I realized that this is what makes me happiest. I’m a
+        natural problem solver, creative, and techie. Combining my skills and
+        talents into a career is my number one goal, and I’m looking to join an
+        innovative organization that’s working to solve problems both big and
+        small.`}}
+      </p>
     </>
   );
 }
