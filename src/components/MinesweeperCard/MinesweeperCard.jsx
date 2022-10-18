0e0e0695fff3c35a47b49311f7a446245a21fe7e
jshprintz <jshprintz@gmail.com>
2022-10-18 13:33:51 -0700

    imported lazy load for images

--- src/components/MinesweeperCard/MinesweeperCard.jsx
+++ src/components/MinesweeperCard/MinesweeperCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import LazyLoad from "react-lazy-load";
 
 const MinesweeperCard = () => (
   
   <Card raised href="/minesweeper" centered style={{ width: 350 }}>
-    <img src="https://i.imgur.com/V0XIXJ1.png" alt="Minesweeper"/>
+    <LazyLoad width={350} offset={10}>
+    <img className="card-image" src="https://i.imgur.com/V0XIXJ1.png" alt="Minesweeper"/>
+    </LazyLoad>
     <Card.Content>
       <Card.Header>Mega Minesweeper</Card.Header>
       <Card.Meta>Created 2022</Card.Meta>
