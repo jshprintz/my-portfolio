d8bfca4a2f58bdebc89dd725014fc921687f397f
jshprintz <jshprintz@gmail.com>
2022-10-18 14:35:58 -0700

    more lazy loading badges

--- src/pages/MinesweeperPage/MinesweeperPage.jsx
+++ src/pages/MinesweeperPage/MinesweeperPage.jsx
@@ -67,26 +67,34 @@ class MinesweeperPage extends Component {
             </Segment>
           </Grid.Row>
           <Grid.Row>
+          <LazyLoad height={40} offset={100} >
             <img
               src="https://camo.githubusercontent.com/e6b67b27998fca3bccf4c0ee479fc8f9de09d91f389cccfbe6cb1e29c10cfbd7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f637373332d2532333135373242362e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d63737333266c6f676f436f6c6f723d7768697465"
               alt="CSS3"
               className="badge fourth-animation"
             />
+            </LazyLoad>
+            <LazyLoad height={40} offset={100} >
             <img
               src="https://camo.githubusercontent.com/49fbb99f92674cc6825349b154b65aaf4064aec465d61e8e1f9fb99da3d922a1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f68746d6c352d2532334533344632362e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465"
               alt="HTML5"
               className="badge fourth-animation"
             />
+            </LazyLoad>
+            <LazyLoad height={40} offset={100} >
             <img
               src="https://camo.githubusercontent.com/aeddc848275a1ffce386dc81c04541654ca07b2c43bbb8ad251085c962672aea/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145"
               alt="Javascript"
               className="badge fourth-animation"
             />
+            </LazyLoad>
+            <LazyLoad height={40} offset={100} >
             <img
               src="https://camo.githubusercontent.com/f6d50128cb007f85916b7a899da5d94f654dce35a37331c8d28573aef46f4274/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6769746875622d2532333132313031312e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d676974687562266c6f676f436f6c6f723d7768697465"
               alt="GitHub"
               className="badge fourth-animation"
             />
+            </LazyLoad>
           </Grid.Row>
         </Grid>
         <Footer />
